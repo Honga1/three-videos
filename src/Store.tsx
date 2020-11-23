b959df5f40ed656eb14a50a48182d1b9ff870207
Jae Perris <jae.perris@gmail.com>
2020-11-23 17:46:00 +0100

    Add state to store

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,6 +1,11 @@
 import create from 'zustand/vanilla';
 import createStoreHook from 'zustand';
 
+type Recording = {
+  blob: Blob;
+  url: string;
+};
+
 interface Streams {
   video: MediaStream;
   audio: MediaStream;
@@ -8,14 +13,24 @@ interface Streams {
 
 type State = {
   streams: Streams | undefined;
-  isUserStreamOn: boolean;
+  staticVideos: { start: Blob; middle: Blob; end: Blob } | undefined;
+  recordings: { video?: Recording; audio?: Recording };
+  fakedRecording: Blob | undefined;
+  isPlaybackReady: boolean;
   setStreams: (streams: Streams) => void;
   closeStreams: () => void;
+  setVideoRecording: (recording: Recording) => void;
+  setStaticVideos: (start: Blob, middle: Blob, end: Blob) => void;
+  setFakedRecording: (video: Blob) => void;
+  setPlaybackReadiness: (isReady: boolean) => void;
 };
 
 export const store = create<State>((set, get) => ({
   streams: undefined,
-  isUserStreamOn: false,
+  recordings: {},
+  fakedRecording: undefined,
+  staticVideos: undefined,
+  isPlaybackReady: false,
   setStreams: (streams: Streams) => {
     get().closeStreams();
     const { audio, video } = streams;
@@ -32,7 +47,7 @@ export const store = create<State>((set, get) => ({
       });
     });
 
-    set({ streams, isUserStreamOn: true });
+    set({ streams });
   },
   closeStreams: () => {
     const maybeStreams = get().streams;
@@ -45,8 +60,20 @@ export const store = create<State>((set, get) => ({
         track.stop();
       });
     }
-    set({ isUserStreamOn: false });
+    set({ streams: undefined });
   },
+
+  setVideoRecording: (recording) => {
+    set({ recordings: { video: recording } });
+  },
+  setStaticVideos: (start, middle, end) => {
+    set({ staticVideos: { start, middle, end } });
+  },
+  setFakedRecording: (video) => {
+    set({ fakedRecording: video });
+  },
+
+  setPlaybackReadiness: (isReady: boolean) => set({ isPlaybackReady: isReady }),
 }));
 
 export const useStore = createStoreHook(store);
