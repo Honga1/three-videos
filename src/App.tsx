18a42fd6514de1acc9cd0f7bf0ec46cd350763e3
Jae Perris <jae.perris@gmail.com>
2020-11-23 17:49:32 +0100

    Implement Ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
-import { Permissions } from './Permissions';
-import { Videos } from './Videos';
+import { DevUi } from './DevUi';
+import { SequentialVideos } from './SequentialVideos';
+import { useStore } from './Store';
+import { UserUi } from './UserUi';
 
 function App(): React.ReactElement {
+  const staticVideos = useStore((state) => state.staticVideos);
+  const fakedRecording = useStore((state) => state.fakedRecording);
+
   return (
     <div className="App">
       <div className="AppContents">
-        <Permissions />
-        {/* <Videos></Videos> */}
+        <UserUi />
+        <DevUi />
+        {!!staticVideos && !!fakedRecording && (
+          <SequentialVideos
+            start={staticVideos.start}
+            middle={fakedRecording}
+            end={staticVideos.end}
+          />
+        )}
       </div>
     </div>
   );
