fb2e080293eaf8d50aba94bc9c94a2539c22845f
Jae Perris <jae.perris@gmail.com>
2020-11-23 17:47:03 +0100

    Add styles to messages component

--- src/Messages.tsx
+++ src/Messages.tsx
@@ -1,11 +1,33 @@
+import './Messages.css';
+
 export const ErrorMessage = ({ reason }: { reason: string }): React.ReactElement => {
-  return <div className="Error">Error: {reason}</div>;
+  return (
+    <div className="Message ErrorMessage">
+      <span>Error: {reason}</span>
+    </div>
+  );
 };
 
 export const PromptMessage = ({ text }: { text: string }): React.ReactElement => {
-  return <div className="Prompt">{text}</div>;
+  return (
+    <div className="Message PromptMessage">
+      <span>{text}</span>
+    </div>
+  );
 };
 
 export const SuccessMessage = ({ text }: { text: string }): React.ReactElement => {
-  return <div className="Success">Success: {text}</div>;
+  return (
+    <div className="Message SuccessMessage">
+      <span>Success: {text}</span>
+    </div>
+  );
+};
+
+export const NeutralMessage = ({ text }: { text: string }): React.ReactElement => {
+  return (
+    <div className="Message NeutralMessage">
+      <span>{text}</span>
+    </div>
+  );
 };
