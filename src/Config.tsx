974a4e8c7e7888d10465ed4c3c019b6880a3a5df
Jae Perris <jae.perris@gmail.com>
2020-11-24 12:34:39 +0100

    Add config component

---
+++ src/Config.tsx
@@ -0,0 +1,40 @@
+import { ChangeEvent, ReactElement } from 'react';
+import { LabelledInput } from './LabelledInput';
+import { store } from './Store';
+
+export const Config = (): ReactElement => {
+  const onDurationChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const config = store.getState().config;
+    const recordingDuration = parseInt(event.target.value, 10);
+
+    store.setState({ config: { ...config, recordingDuration } });
+  };
+
+  const onApiUrlChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const config = store.getState().config;
+    const apiUrl = event.target.value;
+
+    store.setState({ config: { ...config, apiUrl } });
+  };
+  return (
+    <div className="Config">
+      <LabelledInput
+        label="Recording Duration:"
+        name="recordingDuration"
+        type="number"
+        min={0}
+        max={16}
+        defaultValue={store.getState().config.recordingDuration}
+        onChange={onDurationChange}
+      ></LabelledInput>
+
+      <LabelledInput
+        label="API Url:"
+        name="apiUrl"
+        type="string"
+        defaultValue={store.getState().config.apiUrl}
+        onChange={onApiUrlChange}
+      ></LabelledInput>
+    </div>
+  );
+};
