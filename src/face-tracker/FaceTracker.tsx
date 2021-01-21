085898955789baab52e9e02ba2023a3600f413f3
Jae Perris <jae.perris@gmail.com>
2021-01-21 15:01:14 +0100

    Add face to face tracker

--- src/face-tracker/FaceTracker.tsx
+++ src/face-tracker/FaceTracker.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement, useEffect, useRef } from 'react';
 import { Canvas } from 'react-three-fiber';
+import { useAnimationFrame } from '../ChainableAsyncVideos';
 import { store } from '../Store';
 import { KeyPointsOnVideo } from './key-points-on-video';
 import { StoreLoader } from './store-loader';
@@ -12,23 +13,43 @@ type Props = {
 
 export const FaceTracker = ({ stream }: Props): ReactElement => {
   const ref = useRef<HTMLDivElement>(null);
-
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const bothDrawn = useRef<HTMLCanvasElement>(document.createElement('canvas'));
   useEffect(() => {
     const maybeCanvas = ref.current?.getElementsByTagName('canvas')[0];
-    if (!maybeCanvas) return;
+    const maybeVideo = videoRef.current;
+
+    if (!maybeCanvas || !maybeVideo) return;
+
+    bothDrawn.current.width = maybeCanvas.width;
+    bothDrawn.current.height = maybeCanvas.height;
 
     store.setState({
-      detectorCanvas: maybeCanvas,
+      detectorCanvas: bothDrawn.current,
     });
-  }, []);
+  });
+
+  useAnimationFrame(60, () => {
+    const maybeCanvas = ref.current?.getElementsByTagName('canvas')[0];
+    const maybeVideo = videoRef.current;
+
+    if (!maybeCanvas || !maybeVideo) return;
+    const context = bothDrawn.current.getContext('2d')!;
+    const aspect = maybeVideo.videoWidth / maybeVideo.videoHeight;
+    const offsetX = (maybeCanvas.width - maybeCanvas.height / aspect) / 2;
+    context.clearRect(0, 0, bothDrawn.current.width, bothDrawn.current.height);
+    context.drawImage(maybeVideo, offsetX, 0, maybeCanvas.height / aspect, maybeCanvas.height);
+    context.drawImage(maybeCanvas, 0, 0, maybeCanvas.width, maybeCanvas.height);
+  });
 
   return (
     <>
       <div
         ref={ref}
-        style={{ width: '100%', height: '100%', opacity: '0' }}
+        style={{ width: '100%', height: '100%', opacity: '0.0' }}
         className="FaceTracker"
       >
+        <VideoDetector stream={stream} ref={videoRef} />
         <Canvas
           style={{
             width: '100%',
@@ -46,7 +67,6 @@ export const FaceTracker = ({ stream }: Props): ReactElement => {
           <directionalLight position={[0, 0, 0]}></directionalLight>
           <KeyPointsOnVideo />
         </Canvas>
-        <VideoDetector stream={stream} />
       </div>
     </>
   );
