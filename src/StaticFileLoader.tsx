543e713a973994212633a25b969d3374c338bdad
Jae Perris <jae.perris@gmail.com>
2021-01-21 11:58:55 +0100

    Use mp3 audio files

--- src/StaticFileLoader.tsx
+++ src/StaticFileLoader.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 import { ErrorMessage, NeutralMessage, SuccessMessage } from './ui/Messages';
 import { useStore } from './Store';
 import videoStart from './videos/1.mp4';
-import soundStart from './videos/1-sound.wav';
-import soundEnd from './videos/2-sound.wav';
+import soundStart from './videos/1-sound.mp3';
+import soundEnd from './videos/2-sound.mp3';
 import videoEnd from './videos/3.mp4';
 
 export const StaticFileLoader = (): React.ReactElement => {
@@ -20,12 +20,23 @@ export const StaticFileLoader = (): React.ReactElement => {
       setUiState('loading');
 
       try {
-        const start = await loadVideo(videoStart);
-        const soundOne = await loadAudio(soundStart);
-        const soundTwo = await loadAudio(soundEnd);
-        const end = await loadVideo(videoEnd);
+        const start = loadVideo(videoStart);
+        const soundOne = loadAudio(soundStart);
+        const soundTwo = loadAudio(soundEnd);
+        const end = loadVideo(videoEnd);
+        [start, soundOne, soundTwo, end].map((promise) =>
+          promise.then((contents) => console.log(contents)),
+        );
+
+        const promise = await Promise.all([start, soundOne, soundTwo, end]);
+
         setUiState('loaded');
-        setStaticFiles({ start, soundOne, soundTwo, end });
+        setStaticFiles({
+          start: promise[0],
+          soundOne: promise[1],
+          soundTwo: promise[2],
+          end: promise[3],
+        });
       } catch (error) {
         setUiState('error');
         console.warn(`Could not load element ${error?.path[0]?.src}`);
@@ -62,11 +73,11 @@ const loadAudio = async (src: string) => {
   return new Promise<HTMLAudioElement>((resolve, reject) => {
     const audio = document.createElement('audio') as HTMLAudioElement;
     audio.src = src;
-    audio.load();
     audio.onerror = (error) => reject(error);
     audio.oncanplaythrough = () => {
       resolve(audio);
       audio.oncanplaythrough = null;
     };
+    audio.load();
   });
 };
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -9,3 +9,8 @@ declare module '*.wav' {
   const content: string;
   export default content;
 }
+
+declare module '*.mp3' {
+  const content: string;
+  export default content;
+}
