a7f73d711811d84bc44507668b9171fa88912abb
Jae Perris <jae.perris@gmail.com>
2021-01-21 15:00:04 +0100

    Make video us forwarded ref

--- src/face-tracker/video-detector.tsx
+++ src/face-tracker/video-detector.tsx
@@ -1,62 +1,99 @@
 import '@tensorflow/tfjs-backend-webgl';
-import { ReactElement, useEffect, useRef } from 'react';
+import { forwardRef, MutableRefObject, ReactElement, useEffect, useRef } from 'react';
 import { useStore } from '../Store';
 import { getKeyPoints } from './getKeyPoints';
 import { store } from './store';
 import { store as baseStore } from '../Store';
 import { useAnimationFrameAsync } from './use-animation-frame';
 
-export const VideoDetector = ({ stream }: { stream: MediaStream }): ReactElement => {
-  const ref = useRef<HTMLVideoElement | null>(document.createElement('video'));
-  const hasLoadedData = useRef(false);
-  const enabled = useStore((state) => state.currentPlaybackTrack === 1);
-
+const useForwardedRef = <T extends any>(
+  ref: ((instance: T | null) => void) | MutableRefObject<T | null> | null,
+) => {
+  const innerRef = useRef<T>(null);
   useEffect(() => {
-    const video = ref.current;
-    if (!video) return;
-    video.srcObject = stream;
-    video.play();
-    video.addEventListener('loadeddata', () => {
-      hasLoadedData.current = true;
-    });
-  }, [stream]);
-
-  useEffect(() => {
-    const main = async () => {
-      const video = ref.current;
-      if (video === null || hasLoadedData.current === false) return;
-      video.width = stream.getTracks()[0]!.getSettings().width!;
-      video.height = stream.getTracks()[0]!.getSettings().height!;
-      const keyPoints = await getKeyPoints(video);
-      keyPoints && store.getState().setNormalizedMesh(keyPoints);
-    };
-
-    main();
-  }, [stream]);
-
-  const isTracking = () => baseStore.getState().isTracking;
-  useAnimationFrameAsync(25, async () => {
-    const main = async () => {
-      if (!enabled && !isTracking()) {
-        return;
-      }
-
-      if (!enabled && isTracking()) {
-        baseStore.getState().setIsTracking(false);
-        return;
-      }
-
-      const video = ref.current;
-      if (video === null || hasLoadedData.current === false) return;
-      video.width = stream.getTracks()[0]!.getSettings().width!;
-      video.height = stream.getTracks()[0]!.getSettings().height!;
-
-      const keyPoints = await getKeyPoints(video);
-      keyPoints && store.getState().setNormalizedMesh(keyPoints);
-      if (!isTracking()) baseStore.getState().setIsTracking(true);
-    };
-
-    main();
+    if (!ref) return;
+    if (typeof ref === 'function') {
+      ref(innerRef.current);
+    } else {
+      // eslint-disable-next-line no-param-reassign
+      ref.current = innerRef.current;
+    }
   });
-  return <></>;
+
+  return innerRef;
 };
+
+export const VideoDetector = forwardRef<HTMLVideoElement, { stream: MediaStream }>(
+  ({ stream }: { stream: MediaStream }, ref): ReactElement => {
+    const videoRef = useForwardedRef(ref);
+
+    const hasLoadedData = useRef(false);
+    const enabled = useStore((state) => true);
+
+    useEffect(() => {
+      const video = videoRef.current;
+      if (!video) return;
+      video.srcObject = stream;
+      video.play();
+      video.addEventListener('loadeddata', () => {
+        hasLoadedData.current = true;
+      });
+    }, [stream, videoRef]);
+
+    useEffect(() => {
+      const main = async () => {
+        const video = videoRef.current;
+        if (video === null || hasLoadedData.current === false) return;
+        video.width = stream.getTracks()[0]!.getSettings().width!;
+        video.height = stream.getTracks()[0]!.getSettings().height!;
+        const keyPoints = await getKeyPoints(video);
+        keyPoints && store.getState().setNormalizedMesh(keyPoints);
+      };
+
+      main();
+    }, [stream, videoRef]);
+
+    const isTracking = () => baseStore.getState().isTracking;
+    useAnimationFrameAsync(25, async () => {
+      const main = async () => {
+        if (!enabled && !isTracking()) {
+          return;
+        }
+
+        if (!enabled && isTracking()) {
+          baseStore.getState().setIsTracking(false);
+          return;
+        }
+
+        const video = videoRef.current;
+        if (video === null || hasLoadedData.current === false) return;
+        video.width = stream.getTracks()[0]!.getSettings().width!;
+        video.height = stream.getTracks()[0]!.getSettings().height!;
+
+        const keyPoints = await getKeyPoints(video);
+
+        keyPoints && store.getState().setNormalizedMesh(keyPoints);
+        if (!isTracking()) baseStore.getState().setIsTracking(true);
+      };
+
+      main();
+    });
+    return (
+      <video
+        style={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'contain',
+          position: 'absolute',
+          transform: 'translate(-50%, -50%)',
+          left: '50%',
+          top: '50%',
+        }}
+        width="100%"
+        autoPlay
+        height="100%"
+        ref={videoRef}
+      ></video>
+    );
+  },
+);
